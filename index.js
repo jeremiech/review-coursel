aec5c53da29de8147c3d9bd9d6f604741b4308c1
Jeje <jeremiech06@gmail.com>
2023-06-14 15:05:23 +0200

    commit index

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+let someInteger = 129;
 
-function deepCloning(obj){
-    let newObj={...obj}
-    for(let i in newObj){
-        if(typeof newObj[i]==='object'){
-            newObj[i]=deepCloning(newObj[i])
-        }
+function swapDigits(arr){
+    let digits=arr.toString().split('')
+    if(digits.length%2==0)
 
+{
+    
+    for(let i=0;i<digits.length-1;i+=2){
+        const tmp=digits[i]
+        digits[i]=digits[i+1]
+        digits[i+1]=tmp
     }
-    return newObj
+}else{
+    for(let i=1;i<digits.length-1;i+=2){
+        const tmp=digits[i]
+        digits[i]=digits[i+1]
+        digits[i+1]=tmp
+    }
 }
-// let circle1 = {
-//     radius: 100,
-//     center: {
-//         x: 100,
-//          y: 100
-//     }};
-//     console.log(deepCloning(circle1))
+    return Number.parseInt(digits.join(""))
+}
+
+  console.log(swapDigits(someInteger))
