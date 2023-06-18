eb78b3e5d431ea19d9c6288dca5fb14e0c46e2ef
Jeje <jeremiech06@gmail.com>
2023-06-18 22:03:15 +0200

    stage index file with sum of difference among array element

--- ojemba/index.js
+++ ojemba/index.js
@@ -1,4 +1,10 @@
-function joinStrings(string1, string2){
-   // code here
-  return `${string1} ${string2}`
+function sumOfDifferences(arr) {
+    var sum=0
+    let sortted=arr.sort((a,b)=>b-a)
+    for(let i=1;i<sortted.length;i++){
+        let diff_sum=sortted[i]-sortted[i-1]
+        sum+=diff_sum
+    }
+    return Math.abs(sum)
 }
+console.log(sumOfDifferences([2,10,1]))
