9baeb330b1788919b9bb294e63fdfae4f49d711a
Jeje <jeremiech06@gmail.com>
2023-07-02 23:12:53 +0200

    stage the file with reviewrs

---
+++ responsive-nav/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    screens:{
+      'xl':'1280px',
+      'xs':'390px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
